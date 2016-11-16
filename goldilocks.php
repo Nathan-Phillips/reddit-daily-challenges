114accbb7475b0d03994621367a711d1e8d22342
Nathan-Phillips <philip.nobo@gmail.com>
2016-11-16 16:31:19 +0000

    added_array_functions
    Functions to format array printing and create an associative array

--- goldilocks.php
+++ goldilocks.php
@@ -19,6 +19,13 @@
         
         <?php 
 
+        function print_array($array) {
+            // Print a formatted array:
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        }
+
         $input = "100 80 30 50 130 75 90 60 150 85 120 70 200 200 110 100";
 
         $table = explode(' ', $input);
@@ -26,7 +33,17 @@
         $weight = array_shift($table);
         $temperature = array_shift($table);
 
-        print_r($table);
+        $weights = array();
+        $temps = array();
+
+        $i = 0;
+
+        while($i < count($table)){
+            array_push($weights, (array_slice($table, $i, 2)));
+            $i += 2;
+        }
+
+        print_array($weights);
 
         echo '<br>Weight is equal to ' . $weight . '.';
         echo '<br>Temperature is equal to ' . $temperature . '.';
